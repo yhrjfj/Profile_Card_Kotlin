c74c044b094ffcf1f853c8e837b0cf3e904b4b3b
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-20 11:53:36 +0600

    Profile Image

--- app/src/main/java/yhr/jfj/profile_card_kotlin/MainActivity.kt
+++ app/src/main/java/yhr/jfj/profile_card_kotlin/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
