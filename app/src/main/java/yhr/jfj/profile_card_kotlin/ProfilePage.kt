0ba3ecda6d2d75d0256bed0153238db289911abd
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-20 18:18:41 +0600

    Profile Status

--- app/src/main/java/yhr/jfj/profile_card_kotlin/ProfilePage.kt
+++ app/src/main/java/yhr/jfj/profile_card_kotlin/ProfilePage.kt
@@ -2,8 +2,11 @@ package yhr.jfj.profile_card_kotlin
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
@@ -14,14 +17,17 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun ProfilePage() {
-    Column(horizontalAlignment = Alignment.CenterHorizontally,
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxWidth(),
-        ) {
+    ) {
         Image(
             painter = painterResource(id = R.drawable.profile_picture),
             contentDescription = "Profile Picture",
@@ -33,6 +39,25 @@ fun ProfilePage() {
         )
         Text(text = "Anime")
         Text(text = "Japan")
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(text = "150", fontWeight = FontWeight.Bold)
+                Text(text = "Following")
+            }
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(text = "500", fontWeight = FontWeight.Bold)
+                Text(text = "Follower")
+            }
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(text = "20", fontWeight = FontWeight.Bold)
+                Text(text = "Post")
+            }
+        }
     }
 }
 
