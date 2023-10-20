8f5a93cbadc0cf18cd455d829b88a2587218657d
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-20 17:56:08 +0600

    Adjust with full width

--- app/src/main/java/yhr/jfj/profile_card_kotlin/ProfilePage.kt
+++ app/src/main/java/yhr/jfj/profile_card_kotlin/ProfilePage.kt
@@ -1,22 +1,43 @@
 package yhr.jfj.profile_card_kotlin
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun ProfilePage(){
-    Column {
-        Image(painter = painterResource(id = R.drawable.profile_picture), contentDescription = "Profile Picture")
+fun ProfilePage() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth(),
+        ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Profile Picture",
+            modifier = Modifier
+                .size(200.dp)
+                .clip(CircleShape)
+                .border(width = 2.dp, color = Color.Red, shape = CircleShape),
+            contentScale = ContentScale.Crop
+        )
         Text(text = "Anime")
         Text(text = "Japan")
     }
 }
+
 @Preview(showBackground = true)
 @Composable
-fun ProfilePagePreview(){
+fun ProfilePagePreview() {
     ProfilePage()
 }
