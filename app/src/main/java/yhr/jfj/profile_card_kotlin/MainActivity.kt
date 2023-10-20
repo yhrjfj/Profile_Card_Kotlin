d24f1167d97c03607ae211c4150409595aeacdbb
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-20 08:42:44 +0600

    Minor change

--- app/src/main/java/yhr/jfj/profile_card_kotlin/MainActivity.kt
+++ app/src/main/java/yhr/jfj/profile_card_kotlin/MainActivity.kt
@@ -1,18 +1,32 @@
 package yhr.jfj.profile_card_kotlin
 
 import android.os.Bundle
+import android.view.RoundedCorner
 import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardDefaults.cardColors
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.core.content.pm.ShortcutInfoCompat
 import yhr.jfj.profile_card_kotlin.ui.theme.Profile_Card_KotlinTheme
 
@@ -22,7 +36,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             Profile_Card_KotlinTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     createBizCard()
                 }
             }
@@ -31,7 +48,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun createBizCard(){
+fun createBizCard() {
 
 }
 
