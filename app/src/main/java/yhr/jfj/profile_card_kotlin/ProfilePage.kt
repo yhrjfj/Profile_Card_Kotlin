29a24bedf3c475e44f6600d37af2e89b7e274ec8
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-20 17:16:39 +0600

    Add something in ProfilePage

--- app/src/main/java/yhr/jfj/profile_card_kotlin/ProfilePage.kt
+++ app/src/main/java/yhr/jfj/profile_card_kotlin/ProfilePage.kt
@@ -1,2 +1,22 @@
 package yhr.jfj.profile_card_kotlin
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ProfilePage(){
+    Column {
+        Image(painter = painterResource(id = R.drawable.profile_picture), contentDescription = "Profile Picture")
+        Text(text = "Anime")
+        Text(text = "Japan")
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun ProfilePagePreview(){
+    ProfilePage()
+}
